0cd992eb9ce6b6a6530e436e37f5286cf1ad0a63
gracile-fr <gracile@gmx.com>
2012-09-09 02:07:51 +0200

    Thanks to Frank, this works (but the code is still ugly)

--- chrome/content/zotero/itemPane.js
+++ chrome/content/zotero/itemPane.js
@@ -40,6 +40,7 @@ var ZoteroItemPane = new function() {
 		}
 		
 		_itemBox = document.getElementById('zotero-editpane-item-box');
+		_notesBox = document.getElementById('zotero-editpane-notes-box');
 		_notesLabel = document.getElementById('zotero-editpane-notes-label');
 		_notesButton = document.getElementById('zotero-editpane-notes-add');
 		_notesList = document.getElementById('zotero-editpane-dynamic-notes');
@@ -87,6 +88,10 @@ var ZoteroItemPane = new function() {
 		switch (index) {
 			case 0:
 				var box = _itemBox;
+				break;
+			
+			case 1:
+				var box = _notesBox;
 				break;
 			
 			case 2:
@@ -160,14 +165,14 @@ var ZoteroItemPane = new function() {
 			}
 			
 			_updateNoteCount();
-			return;
-		}
-		
-		if (mode) {
-			box.mode = mode;
-		}
-		else {
-			box.mode = 'edit';
+			} else {
+			
+			if (mode) {
+				box.mode = mode;
+			}
+			else {
+				box.mode = 'edit';
+			}
 		}
 		box.item = item;
 		
@@ -221,7 +226,7 @@ var ZoteroItemPane = new function() {
 	
 	
 	function _updateNoteCount() {
-		c = _notesList.childNodes.length;
+		var c = _notesList.childNodes.length;
 		
 		var str = 'pane.item.notes.count.';
 		switch (c){
